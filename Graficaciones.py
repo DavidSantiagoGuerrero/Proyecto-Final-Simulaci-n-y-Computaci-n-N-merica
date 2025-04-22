6e22524f15e323f8bd70e002f206d654c552e9d9
David Guerrero <dsgd2005@gmail.com>
2025-04-21 20:47:50 -0500

    Más o menos arreglado

--- Graficaciones.py
+++ Graficaciones.py
@@ -1,19 +1,10 @@
 import matplotlib.pyplot as plt
 
-def Graficar_Matriz(vx_final):
+def Graficar_Matriz(matriz, titulo, origen):
     plt.figure(figsize=(12, 6))
-    plt.imshow(vx_final, cmap='jet', interpolation='nearest', origin='lower', aspect='auto')
+    plt.imshow(matriz, cmap='jet', interpolation='nearest', origin=origen, aspect='auto')
     plt.colorbar(label='v_x')
-    plt.title('Distribución final de v_x', fontsize=14)
-    plt.xlabel('Índice i', fontsize=12)
-    plt.ylabel('Índice j', fontsize=12)
-    plt.show()
-
-def Graficar_Jacobiano(J):
-    plt.figure(figsize=(12, 6))
-    plt.imshow(J, cmap='jet', interpolation='nearest', origin='upper', aspect='auto')
-    plt.colorbar(label='J')
-    plt.title('Gráfica de J(X)', fontsize=14)
+    plt.title(titulo, fontsize=14)
     plt.xlabel('Índice i', fontsize=12)
     plt.ylabel('Índice j', fontsize=12)
     plt.show()
